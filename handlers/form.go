382a5995ce8ccbb3f8c1bfab053b1fea22336b63
Marco Rosner <marco.rosner@gmail.com>
2023-08-09 18:33:43 -0300

    feat: Add date validation

--- handlers/form.go
+++ handlers/form.go
@@ -10,18 +10,24 @@ import (
 )
 
 type Client struct {
-	Name     string    `form:"name"`
-	Address  string    `form:"address"`
-	Birthday time.Time `form:"birthday" time_format:"10-03-2023" time_utc:"1"`
+	Name         string    `form:"name"`
+	Address      string    `form:"address"`
+	Birthday     time.Time `form:"birthday" binding:"required"` // Format: 2022-10-03T00:00:00Z
+	PaymentDate  time.Time `form:"payment_date" binding:"required"`
+	DeliveryDate time.Time `form:"delivery_date" binding:"required,gtefield=PaymentDate"`
 }
 
 func AddClient(c *gin.Context) {
 	var client Client
 
-	if c.ShouldBind(&client) == nil {
+	if err := c.ShouldBind(&client); err != nil {
 		log.Println(client.Name)
 		log.Println(client.Address)
 		log.Println(client.Birthday)
+		log.Println(client.DeliveryDate)
+		log.Println(client.PaymentDate)
+
+		c.JSON(http.StatusBadRequest, gin.H{"ValidationError": err.Error()})
 	}
 
 	jsonClient, err := json.Marshal(client)
